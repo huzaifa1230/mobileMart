0daa28adc64bdf037e6d165198ff2342b988a5a9
huzaifa1230 <huzaifairshad173@gmail.com>
2024-05-17 17:11:47 +0500

    navbar update

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Home() {
-  return <div className="pl-[360px] bg-gray-600 ">This is home</div>;
+  return (
+    <div className="pl-[360px] bg-black ">
+      This is home
+      <div className="join grid grid-cols-2 w-60 pt-12">
+        <button className="join-item btn btn-outline text-red-300">
+          Previous
+        </button>
+        <button className="join-item btn btn-outline text-red-300">Next</button>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -4,22 +4,22 @@ function Sidebar() {
   return (
     <div className="drawer ">
       <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content bg-black text-red-600">
+      <div className="drawer-content bg-black text-red-600 pl-6">
         {/* Page content here */}
         <label
           htmlFor="my-drawer"
-          className="btn bg-black text-red-600 border-red-500 drawer-button hover:bg-red-900 hover:text-black"
+          className=" btn bg-black text-red-600 border-red-500 drawer-button hover:bg-red-900 hover:text-black"
         >
           View Categories
         </label>
       </div>
-      <div className="drawer-side pt-[128px]">
+      <div className="drawer-side pt-[80px]">
         <label
           htmlFor="my-drawer"
           aria-label="close sidebar"
           className="drawer-overlay "
         ></label>
-        <ul className="menu p-4 w-80 min-h-full bg-black text-red-600 text-base-content ">
+        <ul className="menu p-4 w-80 min-h-full bg-red-950 text-xl text-red-300 font-bold rounded-md ">
           <h1 className="text-white text-2xl pt-12 pb-3">Categories</h1>
 
           <li>
