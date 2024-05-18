c242c9a43b12203f0ff4a3607f53ed367685b6fb
huzaifa1230 <huzaifairshad173@gmail.com>
2024-05-18 21:38:55 +0500

    changing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./Components/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Sidebar from "../src/Components/Sidebar";
 import Footer from "./Components/Footer";
+import ShopPage from "./Components/ShopPage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<ShopPage />} />
           </Routes>
         </BrowserRouter>
       </div>
